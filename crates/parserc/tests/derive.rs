44aa5c19cb496884a1e9420a88cacf4cd0cfa59e
yayanyang <yayanyang@gmail.com>
2025-11-17 21:53:22 +0800

    parserc: remove `lang` mod.

--- crates/parserc/tests/derive.rs
+++ crates/parserc/tests/derive.rs
@@ -1,1 +1,23 @@
+use parserc::{
+    Kind,
+    chars::{self, CharsInput},
+    syntax::InputSyntaxExt,
+};
 
+use parserc_derive::Syntax;
+
+type TokenStream<'a> = chars::TokenStream<'a, Kind>;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Syntax)]
+#[parserc(token = |c: char| c.is_ascii_alphabetic())]
+struct Ident<I>(pub I)
+where
+    I: CharsInput;
+
+#[test]
+fn test_derive() {
+    assert_eq!(
+        TokenStream::from("hello world").parse(),
+        Ok(Ident(TokenStream::from("hello")))
+    );
+}
